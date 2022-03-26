9a859752cd014d01ea678d0a176e593696a85f8f
Ahmad Raza <79039768+code-never-lie@users.noreply.github.com>
2022-03-27 00:43:27 +0500

    Add files via upload

---
+++ Participation.cpp
@@ -0,0 +1,53 @@
+How To Teach Programming?
+
+1- Object First Approach
+     BlueJ
+
+2- Impartive First, Object Later Approach
+     Alice 3.0 
+       - Faisal Hameed
+       - Raheej
+       - Osama Bin ishrat
+       - Khizar Khalid
+       - Sarmad Raza
+       - Zeeshan
+       - Aysha
+       - Hira
+       - Uzma
+       - Aqsa
+3- Pseudo Language approach
+    Microsft Small Basic
+
+4- Classical Method
+
+5- Mini Languages
+   Karel Genie/ Karal The Robot 
+     - Rab Nawaz
+     - Basit 
+     - Mirza Fraz
+     - Ubaid
+     - Goher
+     -Khurram 
+     - Rabi ul Islam
+     - Osama Abid
+     
+4th April
+
+1- Introduction with History
+
+2- How it will improve programming
+
+3- at least three examples
+
+4- Supporting Universities and schools at what level
+
+
+
+
+
+
+
+
+
+
+
